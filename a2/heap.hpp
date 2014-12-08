d0412dc78b22a28e0bb6b3f87a75890cd5ff33c7
Guest <guest-XZaX4w@billl17.(none)>
2014-12-08 16:02:37 +0100

    added comments

--- a2/heap.hpp
+++ a2/heap.hpp
@@ -13,22 +13,27 @@ template <typename T>
 class Heap : public Binary_tree<T>
 	{
 	public:
-		Heap() {} // standart constructor
+		// standart constructor
+		Heap() {}
 
+		// returns the greater child of a tree node
 		std::shared_ptr<TreeNode<T>> get_greater_child(std::shared_ptr<TreeNode<T>> const& current) const
 		{
 			std::shared_ptr<TreeNode<T>> greater_child;
 			if (!current->is_complete())
 			{
-				greater_child = current->m_lhs;
+				greater_child = current->m_lhs; // if the node has only one child it's always the left child
 			}
 			else
 			{
-				greater_child = current->m_lhs->m_value > current->m_rhs->m_value ? current->m_lhs : current->m_rhs;
+				// compares both childs
+				greater_child = current->m_lhs->m_value > 
+								current->m_rhs->m_value ? current->m_lhs : current->m_rhs;
 			}
 			return greater_child;
 		}
 
+		// swaps the root node down the tree until heap condition is restored
 		void down_heap()
 		{
 			auto current = this->get_root();
@@ -36,6 +41,8 @@ class Heap : public Binary_tree<T>
 			if(!current->is_leaf())
 			{
 				auto next = get_greater_child(current);
+				/* swaps the parent node with the greater child as long as a 
+				child exists and the parent note is smaller then the greater child */
 				while (next != nullptr && current->m_value < next->m_value)
 				{
 					std::swap(current->m_value, next->m_value);
@@ -45,9 +52,11 @@ class Heap : public Binary_tree<T>
 			}
 		}
 
+		// swaps the last node of the tree up the heap until heap condition is restored
 		void up_heap()
 		{
 			auto current = this->m_tree_elements.back();
+			// runs while parent exists and is greater then its parent  
 			while (current->m_parent != nullptr && current->m_value > current->m_parent->m_value)
 			{
 				std::swap(current->m_value, current->m_parent->m_value);
@@ -55,7 +64,7 @@ class Heap : public Binary_tree<T>
 			}
 		}
 
-
+		// adds a node to the tree and moves it to the correct position
 		void insert_node(std::shared_ptr<TreeNode<T>> const& new_node) // adds a node on the correct position
 		{
 			if(this->empty()) // sets root if empty
@@ -66,32 +75,35 @@ class Heap : public Binary_tree<T>
 			else
             {
                 this->add(new_node);
-                new_node->m_parent = *(this->m_tree_elements.begin() + (this->size()-2)/2); // calculates position of parent node and sets pointer to it // size -2 because add() already pushed the node
+                // calculates position of parent node and sets pointer to it // size -2 because add() already pushed the node
+                new_node->m_parent = *(this->m_tree_elements.begin() + (this->size()-2)/2); 
 
-                if (new_node->m_parent->m_lhs == nullptr)
+                if (new_node->m_parent->m_lhs == nullptr) // puts new node as parents lhs if lhs didn't exist before
                 {
-                    new_node->m_parent->m_lhs = new_node;	// sets parent nodes
+                    new_node->m_parent->m_lhs = new_node;
                 }
-                else if (new_node->m_parent->m_rhs == nullptr)
+                else if (new_node->m_parent->m_rhs == nullptr) // puts new node as parents rhs if lhs existed and rhs didn't exist before
                 {
                     new_node->m_parent->m_rhs = new_node;
                 }
-                else
+                else // parent node is already complete
                 {
                     std::cout << "structure error" << std::endl;
                 }
-                new_node->m_depth=new_node->m_parent->m_depth+1;
+                new_node->m_depth=new_node->m_parent->m_depth+1; // depth = parents depth +1
                 up_heap();
             }
 		}
 
+		// sorts the heap in place
 		void sort()
 		{
 
 			auto last_unsorted = this->m_tree_elements.end()-1;
-			while(*last_unsorted != this->get_root())
+			while(*last_unsorted != this->get_root()) // while unsorted elements exist
 			{
-				std::swap(this->get_root()->m_value, (*(last_unsorted))->m_value);
+				std::swap(this->get_root()->m_value, (*(last_unsorted))->m_value); // swap root with last unsorted
+				// remove parents pointer to the node
 				if ((*last_unsorted)->m_parent->m_rhs != nullptr)
 				{
 					(*last_unsorted)->m_parent->m_rhs = nullptr;
@@ -99,11 +111,12 @@ class Heap : public Binary_tree<T>
 				else{
 					(*last_unsorted)->m_parent->m_lhs = nullptr;
 				}
-				down_heap();
-				--last_unsorted;
+				down_heap(); // restore heap condition
+				--last_unsorted; // new last unsorted
 			}
 		}
 
+		// prints the value of the tree nodes in the order of the vector they are stored in
 		void print_vector() const
 		{
 			for (auto it = this->m_tree_elements.begin(); it != this->m_tree_elements.end(); ++it)
@@ -112,13 +125,14 @@ class Heap : public Binary_tree<T>
 			}
 		}
 
+		// generates a Heap from a container with iterator
 		template <typename container>
-		void generate(container const& c) // generates a Heap from a container with iterator
+		void generate(container const& c)
 		{
 			std::shared_ptr<TreeNode<T>> to_add;
 			for (auto i = c.begin(); i != c.end(); ++i)
 			{
-				to_add = std::make_shared<TreeNode<T>>(*i);
+				to_add = std::make_shared<TreeNode<T>>(*i); // create new tree node from container element
 				insert_node(to_add);
 			}
 		}
