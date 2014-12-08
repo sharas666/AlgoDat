ceebd64fda15ccc8f11e7e0b6a88cd5d45700445
Guest <guest-XZaX4w@billl17.(none)>
2014-12-08 16:50:02 +0100

    input

--- a2/main.cpp
+++ a2/main.cpp
@@ -1,33 +1,43 @@
 #include <vector>
 #include <iostream>
 #include "heap.hpp"
-#include <cstdlib>
+#include <cctype>
+#include <algorithm>
+#include <string>
 
-
-
+// checks if all chars of a string are numbers
+bool is_digits(const std::string &str)
+{
+    return std::all_of(str.begin(), str.end(), ::isdigit);
+}
 
 int main(int argc, char const *argv[])
 {
-
-
+    std::string test_string;
+    int to_push;
     std::vector<int> v;
-    for (int i = 30; i > 0; --i)
-    {
-    	v.push_back(rand()%100);
-    }
-    // v.push_back(8);
-    // v.push_back(3);
-    // v.push_back(7);
-    // v.push_back(5);
 
+    for (int i = 1; i <= argc-1; ++i)
+    {
+        test_string = argv[i];
+        // break if input is not a number
+        if (!is_digits(test_string))
+        {
+            std::cout <<"input is not a number" << std::endl;
+            return 0;
+        }
+        // add input to the vector
+        else
+        {
+            to_push = stoi(test_string);
+            v.push_back(to_push);
+        }
+    }
 
     Heap<int> h{};
-    h.generate(v);
-    h.sort();
-  	h.print_vector();
-  	//h.print();
-  	//std::cout << h.get_root()->m_value << std::endl;
-
+    h.generate(v); // insert elements of v into heap
+    h.sort(); // sort heap
+  	h.print_vector(); // print sorted vector
 
 	return 0;
 }
